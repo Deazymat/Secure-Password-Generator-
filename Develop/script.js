1e996234affd64a5e57db638223b14bbba0ac14f
Matthew <Deweasematthew@gmail.com>
2023-04-07 19:11:31 -0500

    updated arrays

---
+++ Develop/script.js
@@ -0,0 +1,125 @@
+// Function to make a password based on the persons input.
+function generatePassword() {
+  //Array of lowercase letters
+  var cat = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  // Array of uppercase letters
+  var monkey = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+  // Array of numbers
+var squirrel = [
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9"];
+// Array of special characters
+var frog = [
+"!",
+"@",
+"#",
+"$",
+"^",
+"&",
+"*",
+"(",
+")",
+"-",
+"_",
+"+",
+"=",
+"/",
+"?",
+":",
+";",
+".",
+">",
+",",
+"<",
+"`",
+"~",
+"[",
+"]",
+"{",
+"}",
+"|",
+];
+
+
+}
+
+
+
+
+
+
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
